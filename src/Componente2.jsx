7a0bf4087c6dce0492f93e87c0795751583f584d
hacarapi <huberacarapi@gmail.com>
2025-12-02 15:25:58 -0400

    Examen Final

--- src/Componente2.jsx
+++ src/Componente2.jsx
@@ -1,14 +1,36 @@
 function Componente2({ titulo = 'Título por defecto', descripcion = 'Descripción por defecto', img1, img2 }){
     return (
-        <section className="componente2 text-center">
-            <div className="images-row">
-                <img src={img1 || 'https://picsum.photos/seed/1/400'} alt="img1" className="circular-img" />
-                <img src={img2 || 'https://picsum.photos/seed/2/400'} alt="img2" className="circular-img" />
-            </div>
+        <section className="componente2">
+            <div className="container-fluid">
+                <div className="row align-items-center">
+                    
+                    <div className="col-12 col-md-3 text-center text-md-start mb-3 mb-md-0">
+                        <h3>CARRERA DE INFORMATICA</h3>
+                        <h5>Materia: Programacion Web II</h5>
+                    </div>
 
-            <div className="componente2-text">
-                <h3>{titulo="Titulo de Examen"}</h3>
-                <p>{descripcion="Esta es la descripcion del examen"}</p>
+                    <div className="col-12 col-md-6">
+                        <div className="images-row d-flex justify-content-center">
+                            <img src="https://img.freepik.com/vector-gratis/ilustracion-dibujos-animados-artesano-dibujada-mano_23-2150765969.jpg?semt=ais_hybrid&w=740&q=80" alt="img1" className="circular-img me-3" />
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN4q_EELq3Do-CLZoIxEBtsF4cZgB1N5eZ8g&s" alt="img2" className="circular-img" />
+                        </div>
+                        <div className="text-center mt-3">
+                            <h3>{titulo}</h3>
+                            <p>{descripcion}</p>
+                        </div>
+                    </div>
+
+                    <div className="col-12 col-md-3 text-center text-md-start mb-3 mb-md-0">
+                        <a href="https://www.facebook.com" target="_blank" rel="noreferrer" className="mx-2 text-dark text-decoration-none social-link">
+                        <i className="fa fa-facebook-official social-icon" aria-hidden="true"></i>
+                        <span className="social-title">Facebook</span>
+                    </a>
+                    <a href="https://www.twitter.com" target="_blank" rel="noreferrer" className="mx-2 text-dark text-decoration-none social-link">
+                        <i className="fa fa-twitter social-icon" aria-hidden="true"></i>
+                        <span className="social-title">Twitter</span>
+                    </a>
+                    </div>
+                </div>
             </div>
         </section>
     )
